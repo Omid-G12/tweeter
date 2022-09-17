646cff6a55861c578a45ab9e030062af6945e4fe
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-17 02:11:11 +0000

    final commit with responisve design for phone and desktop

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,39 +6,13 @@ $(document).ready(function() {
   * Reminder: Use (and do all your DOM work in) jQuery's document ready function
   */
 
-  /*const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]*/
-
-  //RENDER Function WORKS!
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       let output = createTweetElement(tweet);
-      //console.log(output);
+      //use prepend to see new tweets on top!
       $('.all-tweets').prepend(output);
     }
 
@@ -84,21 +58,21 @@ $(document).ready(function() {
   $form.on("submit", (event) => {
     event.preventDefault();
     if ($('#tweet-text').val() === "" || $('#tweet-text').val() === null) {
-      //alert("You can't submit an empty tweet! Try writing something first!")
+
       $('.error p').text("You can't submit an empty tweet! Try writing something first!");
       $('.error').slideDown();
 
     } else if ($('#tweet-text').val().length > 140) {
-      //alert("Tweet character limit exceeded!")
+
       $('.error p').text("Tweet character limit exceeded!");
+
     } else {
+      
       $('.error').slideUp();
       const serializedData = $form.serialize();
       $.post('/tweets', serializedData, (response) => {
-        //console.log(response);
         loadTweets();
       });
-      //loadTweets();
     }
   });
 
